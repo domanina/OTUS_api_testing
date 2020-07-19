b8baec25326172f2d05587926bf4c868cb710415
domanina <len_sfumato@mail.ru>
2020-07-19 18:29:54 +0300

    19.07.20

---
+++ 2_homework/api_testing_breweries.py
@@ -0,0 +1,48 @@
+import pytest
+import random
+
+
+# @pytest.mark.parametrize('input_id, output_id',
+#                          [(10000, '10000'),
+#                              (-1, '-1'),
+#                              (0, '0')])
+# @pytest.mark.parametrize('input_title, output_title',
+#                          [('title', 'title'),
+#                              ('', ''),
+#                              (100, '100'),
+#                              ('&', '&')])
+# def test_api_post_request(api_client, input_id, output_id, input_title, output_title):
+#     res = api_client.post(
+#         path="/posts",
+#         data={'title': input_title, 'body': 'bar', 'userId': input_id})
+#     res_json = res.json()
+#     assert res_json['title'] == output_title
+#     assert res_json['body'] == 'bar'
+#     assert res_json['userId'] == output_id
+
+
+# Параметр фильтрации постов по Id пользователя
+# https://jsonplaceholder.typicode.com/posts?userId=1
+#@pytest.mark.parametrize('userId', [-1, 0, 'a', 11])
+def test_api_empty_response(api_client_brew):
+    res = api_client_brew.get_brew(
+        path="/5745",
+        #params={'userId': userId}
+    )
+    # Проверяем что на таких данных ответ пустой
+    assert res.json() == {"id":5745,"name":"Brewery In Planning - Eugene","brewery_type":"planning","street":"","city":"Eugene","state":"Oregon","postal_code":"97401-7004","country":"United States","longitude":None,"latitude":None,"phone":"","website_url":"","updated_at":"2018-08-11T21:39:11.494Z","tag_list":[]}
+
+
+# Параметр фильтрации постов по Id пользователя
+# https://jsonplaceholder.typicode.com/posts?userId=1
+# @pytest.mark.parametrize('userId, userId_in_response', [(1, 1), (2, 2), (10, 10)])
+# def test_api_filtering(api_client_brew, userId, userId_in_response):
+#     response = api_client_brew.get_brew(
+#         path="/posts",
+#         params={'userId': userId}
+#     )
+#     # Проверка что случайный пост от пользователя с ожидаемым id
+#     response_json = response.json()
+#     random_post_number = random.randint(1, 9)
+#     assert len(response_json) > 0
+#     assert response.json()[random_post_number]['userId'] == userId_in_response
---
+++ 2_homework/conftest.py
@@ -0,0 +1,37 @@
+import pytest
+import requests
+
+
+class APIClient:
+    """
+    Упрощенный клиент для работы с API
+    Инициализируется базовым url на который пойдут запросы
+    """
+
+    def __init__(self, base_address):
+        self.base_address = base_address
+
+    def post_brew(self, path="/", params=None, data=None, headers=None):
+        url = self.base_address + path
+        print("POST request to {}".format(url))
+        return requests.post(url=url, params=params, data=data, headers=headers)
+
+    def get_brew(self, path="/", params=None):
+        url = self.base_address + path
+        print("GET request to {}".format(url))
+        return requests.get(url=url, params=params)
+
+# Тестовое API: https://api.openbrewerydb.org/breweries
+def pytest_addoption(parser):
+    parser.addoption(
+        "--url",
+        action="store",
+        default="https://api.openbrewerydb.org/breweries",
+        help="This is request url"
+    )
+
+
+@pytest.fixture(scope="session")
+def api_client_brew(request):
+    base_url = request.config.getoption("--url")
+    return APIClient(base_address=base_url)
